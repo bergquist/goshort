f1c5e9626e3ae7f212ca3260f3806b603017db07
Carl Bergquist <carl.bergquist@gmail.com>
2015-10-02 15:16:18 +0200

    splitt unittests

--- handlers_test.go
+++ handlers_test.go
@@ -8,30 +8,6 @@ import (
 	"testing"
 )
 
-func TestMissingShortUrl(t *testing.T) {
-	req, _ := http.NewRequest("GET", "http://goshort.com/asdf", nil)
-
-	w := httptest.NewRecorder()
-	ResolveShortUrlHandler(w, req)
-
-	if w.Code != 404 {
-		t.Error("Expected handler to return 404")
-	}
-
-	fmt.Printf("%d - %s \n", w.Code, w.Body.String())
-}
-
-func TestExistnigShortUrl(t *testing.T) {
-	req, _ := http.NewRequest("GET", "http://goshort.com/asdf", nil)
-
-	w := httptest.NewRecorder()
-	ResolveShortUrlHandler(w, req)
-
-	if w.Code != 301 {
-		t.Error("Expected handler to return redirect to full url")
-	}
-}
-
 func TestAddUrlHandler(t *testing.T) {
 	body := strings.NewReader("{\"url\": \"that\"}")
 	req, _ := http.NewRequest("POST", "http://goshort.com/asdf", body)

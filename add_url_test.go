e7d51d0e9fe8d363629b436f4a77e818a1b5c801
Carl Bergquist <carl.bergquist@gmail.com>
2015-10-09 07:07:55 +0200

    adds test that require shortcodes to be unique

--- add_url_test.go
+++ add_url_test.go
@@ -2,11 +2,47 @@ package main
 
 import (
 	"encoding/json"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"strings"
 	"testing"
 )
+
+func parse_result(r io.Reader) (res_create_short_code, error) {
+	decoder := json.NewDecoder(r)
+
+	var res res_create_short_code
+	err := decoder.Decode(&res)
+
+	return res, err
+}
+
+func TestUrlsShouldNotGetSameShortCode(t *testing.T) {
+	fakedb := NewFakeDatabase()
+
+	//create request
+	body := strings.NewReader("{\"url\": \"http://www.grafana.com\"}")
+	req, _ := http.NewRequest("POST", "http://goshort.com/create", body)
+
+	body2 := strings.NewReader("{\"url\": \"http://www.grafana.org\"}")
+	req2, _ := http.NewRequest("POST", "http://goshort.com/create", body2)
+
+	w := httptest.NewRecorder()
+	w2 := httptest.NewRecorder()
+
+	addurlHandler := AddUrlHandlerstruct{client: fakedb}
+
+	addurlHandler.Execute(w, req)
+	addurlHandler.Execute(w2, req2)
+
+	res1, _ := parse_result(w.Body)
+	res2, _ := parse_result(w2.Body)
+
+	if res1.ShortCode == res2.ShortCode {
+		t.Errorf("different urls should not return same shortcode. %s != %s", res1.ShortCode, res2.ShortCode)
+	}
+}
 
 func TestAddUrlHandler(t *testing.T) {
 	fakedb := NewFakeDatabase()
@@ -26,11 +62,7 @@ func TestAddUrlHandler(t *testing.T) {
 	}
 
 	//decode result
-	decoder := json.NewDecoder(w.Body)
-
-	var res res_create_short_code
-	err := decoder.Decode(&res)
-
+	res, err := parse_result(w.Body)
 	if err != nil {
 		t.Error("Cannot decode json result")
 	}
